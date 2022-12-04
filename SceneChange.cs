5808a684b207445b74e91c00cb6c93369b91bf56
oooobeme <117568627+oooobeme@users.noreply.github.com>
2022-12-04 14:18:56 +0500

    Add files via upload

---
+++ SceneChange.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+    public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string sceneName;
+    
+
+    // Update is called once per frame
+    void OnTriggerEnter()
+    {
+        EditorSceneManager.LoadScene(sceneName);
+    }
+}
